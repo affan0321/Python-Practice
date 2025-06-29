686f08977094196a9887c5e057ca4e25531a635c
Affan Tahir <139456008+affan0321@users.noreply.github.com>
2025-06-29 15:18:58 +0500

    practiced inputs and if else statements

---
+++ main.py
@@ -0,0 +1,35 @@
+a = "Affan Tahir"
+
+print(a[0:3:1])
+
+num = (input("Enter your phone number "))
+age = int(input("Enter your age "))
+
+print(f"Your phone number is {num}")
+print(f"Your age is {age}")
+
+print(126 > 130)
+
+print((456==456)!=(235==236))
+
+num1 = int(input("Enter a number"))
+num2 = int(input("Enter a second number"))
+
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)    
+
+gender = (input("Please enter your gender "))
+if gender == "M":
+    print("Male")
+elif gender == "F":
+    print("Female")
+else:
+    print("other")        
+
+num = int(input("Enter a number to check if its even or odd"))
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")    
