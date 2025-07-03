c764fd11bf5363ef9c3e2fcc18474db705f2fc35
Affan Tahir <139456008+affan0321@users.noreply.github.com>
2025-07-03 18:42:28 +0500

    While Loop Practice and created a guessing game using while Loop

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#For Loop Practice
+
 # n=int(input("Enter a number"))
 # fact = 1
 # for i in range(1,n+1):
@@ -29,13 +31,81 @@
 # else:
 #     print("Its not a perfect number")    
 
-n = int(input("Enter a number to check if its a prime number or not"))
-count = 0
-for i in range(1,n+1):
-    if n % i == 0:
-        count = count + 1
+# n = int(input("Enter a number to check if its a prime number or not"))
+# count = 0
+# for i in range(1,n+1):
+#     if n % i == 0:
+#         count = count + 1
 
-if count <=2:
-    print("Its a prime number")
+# if count <=2:
+#     print("Its a prime number")
+# else:
+#     print("It is not a prime number")   
+
+
+
+# a = "bob"
+# b = ""
+# for i in range(len(a)-1,-1,-1):
+#     b = b + a[i]
+# if b == a :
+#     print("It is palindrome")
+# else:
+#     print("It is not a palindrome")        
+
+# print(b)    
+
+# a = "diqdpfh1343224??>x'"
+# char = 0
+# dig = 0
+# spchr = 0
+
+# for i in a:
+#     if i.isdigit():
+#         dig +=1
+#     elif i.isalpha():
+#         char +=1
+#     else:
+#         spchr +=1
+
+# print(f"Your digits are {dig},your alphabets are {char} and special chrachters are {spchr}")                
+
+#While Loop practice
+
+a = 132
+copy = a
+rev = 0
+while a > 0:
+    rev = rev * 10 + a % 10
+    a = a//10
+if copy == rev:
+    print("palindrome")
 else:
-    print("It is not a prime number")    
+    print("Not a palindrome")        
+
+print(rev)    
+
+import random
+
+num = random.randint(1,10)
+tries = 0
+
+while True:
+    guess = int(input("Guess a number between 1 to 10"))
+    if guess == num:
+        tries +=1
+        print(f"Congratulations! you have guessed the right number in {tries} tries ")
+        break
+    
+    elif num < guess:
+        tries += 1
+        print("go a little lower")
+    
+    elif num > guess:
+        tries +=1
+        print("go a little higher")
+
+    else:
+        tries +=1
+        print("OOPS! Try again")    
+
