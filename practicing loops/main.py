1d08e116afd8fe8d64ad0c10c87d94122ab4ff86
Affan Tahir <139456008+affan0321@users.noreply.github.com>
2025-07-06 17:10:38 +0500

    Add files via upload

---
+++ practicing loops/main.py
@@ -0,0 +1,30 @@
+#For Loop practice questions
+#Question 1: Print "Hello World" 10 times using a for loop
+
+# num = int(input("Enter a number: "))
+# for i in range(num):
+#     print("Hello World")
+
+
+#Question 2: Print all natural numbers less than a given number using a for loop
+
+# num = int(input("Enter a number(natural numbers only): "))
+# for i in range(1,num):
+#     print(i)    
+
+
+#Question 3: Print all even numbers between 1 and a given number using a for loop
+
+# for i in range(10,0,-1):
+#     print(i)
+
+#Question 4: Print the multiplication table of a given number using a for loop
+
+# num = int(input("Enter a number for its table: "))
+
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num * i}")
+
+
+num = int(input("Enter a number : "))
+sum = 0
